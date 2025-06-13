2aca7c345c1c049954d5ac8186d932066b3ff064
Ram Bagga <rambagga@Rams-MacBook-Air.local>
2025-06-13 19:37:42 +0530

    fix(): New animations

--- src/views/background.js
+++ src/views/background.js
@@ -1,9 +1,14 @@
 import React, {useEffect, useRef} from "react";
 
 import SmoothScrollTo from "../hooks/smoothScrollTo";
-import ASCIIText from '../styles/AsciiText';
+import BlurText from "../styles/BlurText";
+import TextPressure from "../styles/TextPressure";
 
 import WOW from "wowjs";
+
+const handleAnimationComplete = () => {
+  console.log('Animation completed!');
+};
 
 export default function Background() {
 
@@ -18,9 +23,20 @@ export default function Background() {
 			<div id="stars2" />
 			<div id="stars3" />
 			<div className="top-container flex">
-					<ASCIIText   text='Hello, I&apos;m Ram Bagga.
-					I&apos;m a Software Developer, Full-stack proficient.'
-					enableWaves={true}asciiFontSize={8}/>
+				 <TextPressure
+    text="Hi I'm Ram Bagga"
+    flex={true}
+    alpha={false}
+    stroke={false}
+    width={true}
+    weight={true}
+    italic={true}
+    textColor="#ffffff"
+    strokeColor="#ff0000"
+    minFontSize={10}
+  />
+			</div>
+			<div className="bottom-container flex" alignItems="center" justifyContent="bottom">
 				<button className={"work-button wow bounceIn"} data-wow-offset="0"
 					onClick={() => SmoothScrollTo("my-projects")}>
 					View my work
